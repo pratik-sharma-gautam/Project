8599fa6c69ca8b340530152d2fcc6ad33e3f55aa
Pratik Sharma <pratiksharmagautam@gmail.com>
2024-03-09 08:34:26 +0545

    Kind of Working page created, just need knowledge of deep science then we are done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,12 +8,22 @@ function Navbar() {
         <header className="text-gray-300 body-font">
           <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-              </svg>
+              </svg> */}
+<Link href={"https://www.chess.com/member/pratiksharma-yt"}>
+<svg width="40" height="40" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="50" cy="50" r="50" fill="#EFEFEF" />
+  <path d="M 25 75 L 50 25 L 75 75 L 55 55 L 50 75 L 45 55 Z" fill="#000" />
+</svg></Link>
+
+
+
+
+
             </a>
             <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-              <Link href={'/Home'} className="mr-5 hover:text-blue-700 hover:cursor-pointer">Home</Link>
+              <Link href={'/'} className="mr-5 hover:text-blue-700 hover:cursor-pointer">Home</Link>
               <Link href="/About" className="mr-5 hover:text-blue-700 hover:cursor-pointer">About</Link>
               <Link href={'/Contact'} className="mr-5 hover:text-blue-700 hover:cursor-pointer">Contact</Link>
             </nav>
